d09ae82cf07b84cde8dbe2f7b94c5ca74ca8de3a
Julian Hahn <julianhahn@macbook-pro.mynetworksettings.com>
2023-01-13 12:31:52 -0500

    versioning

--- stories/components/Button/Button.tsx
+++ stories/components/Button/Button.tsx
@@ -10,11 +10,11 @@ interface Props extends PressableProps {
 const Button = ({ children, style, ...props }: Props) => {
     return (
         <Pressable {...props} style={[styles.container, style]}>
-            {
-                typeof children === 'string' 
-                ? <Text style={styles.text}>{children}</Text> 
-                : children
-            }
+        {
+            typeof children === 'string' 
+            ? <Text style={styles.text}>{children}</Text> 
+            : children
+        }
         </Pressable>
     );
 }
