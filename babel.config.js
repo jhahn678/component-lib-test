6c052bad7483927d9439f97e42bdcbb0a24962a2
Julian Hahn <julianhahn@macbook-pro.mynetworksettings.com>
2023-01-14 17:20:00 -0500

    This babel config should only apply to expo

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ["module:metro-react-native-babel-preset", { disableImportExportTransform: true }]
-      // ['babel-preset-expo', { web: { disableImportExportTransform: true }}],
-      // '@babel/preset-react',
-      // ['@babel/preset-env', { modules: false }]
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
