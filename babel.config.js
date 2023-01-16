21ea5f7ad6c055d788dcab17bc2b34f878290079
Julian Hahn <julianhahn@macbook-pro.mynetworksettings.com>
2023-01-16 11:47:08 -0500

    comment

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-// Expo requires a particular preset for Babel that automatically converts modules
-// In order to compile properly with rollup, we need to preserve modules
+// Expo requires a particular preset for Babel that automatically converts modules.
+// In order to compile properly with rollup, we need to preserve modules.
 // In the build script, we're setting an environment variable to allow us to switch
 // between presets here. A better way probably exists, but my knowledge of Babel/Rollup
 // is pretty shallow.
