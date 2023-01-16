5bc20507db18e8c2396bddda8d92c2e57e63093a
Julian Hahn <julianhahn@macbook-pro.mynetworksettings.com>
2023-01-15 20:59:58 -0500

    removed umd option for now

--- scripts/build.ts
+++ scripts/build.ts
@@ -27,21 +27,16 @@ const { argv }: { argv: any } = yargs(hideBin(process.argv))
     default: true,
     description: 'Generate sourcemap.',
   })
-  .option('minify', {
-    type: 'boolean',
-    default: false,
-    description: 'Minify build (used by default with umd)',
-  })
   .option('formats', {
     type: 'string',
     array: true,
-    choices: ['es', 'cjs', 'umd'],
+    choices: ['es', 'cjs'],
     default: ['es', 'cjs'],
     description: "Specify module code generation: 'es', 'cjs'.",
   })
   .example([
     ['$0', 'Bundle web and mobile to es and cjs.'],
-    ['$0 --formats umd cjs', 'Bundle web and mobile to umd and cjs.'],
+    ['$0 --formats cjs', 'Bundle web and mobile to cjs.'],
     ['$0 --package web --analyze', 'Bundle web only to es and cjs and generate analysis files'],
   ]);
 
