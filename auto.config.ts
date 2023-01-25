c12b2ace51ea4e23d927e4740d1a20e024a3f347
Julian Hahn <julianhahn@macbook-pro.mynetworksettings.com>
2023-01-24 22:26:06 -0500

    I think

---
+++ auto.config.ts
@@ -0,0 +1,15 @@
+import { AutoRc} from "@auto-it/core"
+import { INpmConfig } from "@auto-it/npm";
+
+const npmOptions: INpmConfig = {
+  publishFolder: "./build"
+};
+
+/** Auto configuration */
+export default function rc(): AutoRc {
+  return {
+    plugins: [
+      ["npm", npmOptions],
+    ],
+  };
+}
